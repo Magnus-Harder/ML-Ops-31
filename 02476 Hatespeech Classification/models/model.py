db5795a923e45224c5f04a589864ac286dbe1dd5
Magnus Harder <magnusharder@mbptilhdemagnus.students.clients.local>
2024-01-11 13:18:23 +0100

    Draft Model and Training

--- 02476 Hatespeech Classification/models/model.py
+++ 02476 Hatespeech Classification/models/model.py
@@ -1,42 +1,64 @@
 # Imports for the model
-from torch import nn, optim
+from torch import nn, optim, exp
 from sentence_transformers import SentenceTransformer
 from pytorch_lightning import LightningModule
 
-class MyModel(LightningModule):
-    def __init__(self, model_name='aditeyabaral/sentencetransformer-bert-base-cased'):
 
+# Define model dictionary
+model_dict = {'Best' :  'all-mpnet-base-v2',
+              'Fast' : 'all-MiniLM-L6-v2',
+              'Standard' : 'all-distilbert-base-v2'}
+
+# 'aditeyabaral/sentencetransformer-bert-base-cased'
+class HatespeechClassification(LightningModule):
+    def __init__(self, model_type='Fast', hidden_dim=128, activation='relu', dropout=0.2, learning_rate=1e-2, optimizer='Adam'):
         super().__init__()
 
-        self.embedder = SentenceTransformer(model_name)
+        # Define Variables to Determine wheather model is extrapolating on user input
+        self.extrapolation = False
 
+        # Load the Sentence Transformer model
+        self.embedder = SentenceTransformer(model_dict[model_type])
+
+        # Get the embedding size
+        embedding_size = self.embedder.get_sentence_embedding_dimension()
+
+        # Define the Classifier
         self.classifier = nn.Sequential(
-            nn.Linear(768,128),
+            nn.Linear(embedding_size,128),
             nn.ReLU(),
-            nn.Linear(128,2)
+            nn.Linear(128,2),
+            nn.LogSoftmax(dim=1)
         )
-
-        self.loss_fn = nn.CrossEntropyLoss()
-
-    def encode_sentences(self, sentences):
-        embeddings = self.embedder.encode(sentences)
-        return embeddings
+        
+        # Define the loss function
+        self.loss_fn = nn.NLLLoss()
     
+    # Forward pass
     def forward(self, x):
-        if self.train == True:
-            return self.classifier(x)
+        
+        # If Input is encoded, then we are training
+        if self.extrapolation == False:
+            output = self.classifier(x)
         else:
-            return self.classifier(self.embedder.encode(x))
-
-
-        return self.classifier(self.embedder.encode(x, convert_to_tensor = True))
+            # Encode the sentence and convert to probability
+            output = exp(self.classifier(self.embedder.encode(x, convert_to_tensor=True)))
+        
+        return output
     
+    # Training step for pytorch lightning
     def training_step(self,batch):
+
+        # Get the data and target
         data, target = batch
+
+        # Get the predictions and calculate the loss
         pred = self(data)
         loss = self.loss_fn(pred, target)
+        
         return loss
     
+    # Optimizer for pytorch lightning
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr = 1e-2)
 
@@ -45,14 +67,19 @@ if __name__ == '__main__':
     # A simple test to see that the model is loaded correctly:
     sentences = ["This is an example sentence", "Each sentence is converted"]
 
-    my_model = MyModel()
+    my_model = HatespeechClassification()
+    embeddings = my_model.embedder.encode(sentences, convert_to_tensor=True)
 
-    embeddings = my_model.encode_sentences(sentences)
-
-    print(embeddings)
+    print("Embeddings:")
     print(embeddings.shape)
 
-    #Test that model troughput gives sensible results
+    # Test Training forward pass
+    pred_scores = my_model(embeddings)
+    print("Training Prediction scores:")
+    print(pred_scores.shape)
+
+    # Test Prediction forward pass
+    my_model.extrapolation = True
     pred_scores = my_model(sentences)
-    print(pred_scores)
+    print("Prediction scores:")
     print(pred_scores.shape)
--- 02476 Hatespeech Classification/train_model.py
+++ 02476 Hatespeech Classification/train_model.py
@@ -1,6 +1,23 @@
 from pytorch_lightning import Trainer
-from model import MyModel
+from models.model import HatespeechClassification
 
-model = MyModel()
-trainer = Trainer()
-trainer.fit(model)
+import hydra
+
+
+@hydra.main(version_base=None,config_path='conf',config_name="config")
+def train(cfg):
+    print(cfg)
+    hparams = cfg['experiments']
+
+    # Create the model and trainer
+    model = HatespeechClassification(**hparams['class_configuration'])
+    trainer = Trainer(max_epochs=hparams['training_configuration']['max_epochs'])
+
+    # Get Dataloaders
+
+    # Train the model
+    #trainer.fit(model, train_dataloader, val_dataloader)
+
+if __name__ == '__main__':
+    train()
+    
