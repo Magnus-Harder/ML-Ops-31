5a30863a323461461d3a6345016a2be96c180b9e
Magnus Harder <magnusharder@mbptilhdemagnus.students.clients.local>
2024-01-11 09:38:27 +0100

    Model Script placement

--- 02476 Hatespeech Classification/models/model.py
+++ 02476 Hatespeech Classification/models/model.py
@@ -1,26 +1,58 @@
-import torch
+# Imports for the model
+from torch import nn, optim
+from sentence_transformers import SentenceTransformer
+from pytorch_lightning import LightningModule
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
-        
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
+class MyModel(LightningModule):
+    def __init__(self, model_name='aditeyabaral/sentencetransformer-bert-base-cased'):
 
-        Returns:
-            Output tensor with shape [N,out_features]
+        super().__init__()
 
-        """
-        return self.l2(self.r(self.l1(x)))
+        self.embedder = SentenceTransformer(model_name)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(768,128),
+            nn.ReLU(),
+            nn.Linear(128,2)
+        )
+
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def encode_sentences(self, sentences):
+        embeddings = self.embedder.encode(sentences)
+        return embeddings
+    
+    def forward(self, x):
+        if self.train == True:
+            return self.classifier(x)
+        else:
+            return self.classifier(self.embedder.encode(x))
+
+
+        return self.classifier(self.embedder.encode(x, convert_to_tensor = True))
+    
+    def training_step(self,batch):
+        data, target = batch
+        pred = self(data)
+        loss = self.loss_fn(pred, target)
+        return loss
+    
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr = 1e-2)
+
+
+if __name__ == '__main__':
+    # A simple test to see that the model is loaded correctly:
+    sentences = ["This is an example sentence", "Each sentence is converted"]
+
+    my_model = MyModel()
+
+    embeddings = my_model.encode_sentences(sentences)
+
+    print(embeddings)
+    print(embeddings.shape)
+
+    #Test that model troughput gives sensible results
+    pred_scores = my_model(sentences)
+    print(pred_scores)
+    print(pred_scores.shape)
