31fd360309c6ec4e99e7fb3b15dab5629740a2e7
M-Olofsson <mattiasolofsson97@gmail.com>
2024-01-17 14:17:19 +0100

    Add predict_model.py

--- hatespeech_classification_02476/predict_model.py
+++ hatespeech_classification_02476/predict_model.py
@@ -1,8 +1,15 @@
 import torch
+import click
+import os
+from models.model import HatespeechClassification
+import pandas as pd
 
-
-def predict(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader) -> None:
-    """Run prediction for a given model and dataloader.
+@click.command()
+@click.argument("model_file", required=False, default="models/model.pt")
+@click.argument("data_format", required=True, default="string")
+@click.argument("data", required=True)
+def predict(model_file, data_format, data):
+    """Run prediction for a given model and data.
 
     Args:
         model: model to use for prediction
@@ -12,4 +19,21 @@ def predict(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader) -> 
         Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
 
     """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+    prediction_model = HatespeechClassification()
+    prediction_model.load_state_dict(torch.load(model_file))
+    prediction_model.eval()
+    prediction_model.extrapolation = True
+
+    if data_format == "string":
+        predictions = prediction_model(data) > 0.5
+
+    elif data_format == "csv":
+        text_df = pd.read_csv(data)
+        text_data = text_df[text_df.columns[0]].values
+        predictions = prediction_model(text_data) > 0.5
+
+    print(predictions)
+    return predictions
+
+if __name__ == "__main__": 
+    predict() 
