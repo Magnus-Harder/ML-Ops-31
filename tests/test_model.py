807c35b997ce5a2b1eba88e16b1731fa1cab3d75
Amalie Roark <46440775+s184227@users.noreply.github.com>
2024-01-17 14:01:35 +0100

    testing validation step and select optimizer

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,27 @@ from tests import _PATH_DATA, _PROJECT_ROOT, _TEST_ROOT
 import torch
 import os
 import pytest
+from torch.utils.data import DataLoader, TensorDataset
 from hatespeech_classification_02476.models.model import HatespeechClassification
+
+fast_train_data_fpath = os.path.join(_PATH_DATA,"processed", "all-MiniLM-L6-v2", "train_data.pt")
+fast_test_data_fpath = os.path.join(_PATH_DATA,"processed","all-MiniLM-L6-v2", "test_data.pt")
+
+@pytest.fixture
+def sample_data(tmp_path):
+    # Create a temporary directory for storing sample data
+    temp_data_dir = tmp_path / "sample_data"
+    temp_data_dir.mkdir()
+
+    # Create and save sample data
+    sentences = ["This is an example sentence", "This is a fucking stupid sentence"]
+    labels = torch.tensor([0, 1], dtype=torch.float32)
+    torch.save(sentences, temp_data_dir / "train_data.pt")
+    torch.save(labels, temp_data_dir / "train_labels.pt")
+
+    # Return datapath
+    return temp_data_dir
+    #return sentences, labels
 
 def test_model():
 
@@ -36,3 +56,41 @@ def test_model():
     best_scores = best_model(sentences)
     assert pred_scores.shape == torch.Size([2, 1]), "Incorrect dimensionality of \"Fast\" model prediction output"
     assert best_scores.shape == torch.Size([2, 1]), "Incorrect dimensionality of \"Best\" model prediction output"
+
+def test_validation_step(sample_data):
+    model = HatespeechClassification()
+    
+    # Load sample data
+    sentences = torch.load(sample_data / "train_data.pt")
+    labels = torch.load(sample_data / "train_labels.pt")
+    
+    dataset = TensorDataset(model.embedder.encode(sentences, convert_to_tensor=True), labels)
+    dataloader = DataLoader(dataset, batch_size=1)
+    
+    # Ensure the validation step works
+    batch = next(iter(dataloader))
+    loss = model.validation_step(batch)
+    assert isinstance(loss, torch.Tensor)
+
+    '''
+    model = HatespeechClassification()
+    sentences, labels = sample_data
+    #Load data
+    train_data = torch.load(fast_train_data_fpath)
+    test_data = torch.load(fast_test_data_fpath)
+    
+    #dataset = TensorDataset(model.embedder.encode(sentences, convert_to_tensor=True), labels)
+    dataloader = DataLoader(dataset, batch_size=1)
+    
+    # Ensure the validation step works
+    batch = next(iter(dataloader))
+    loss = model.validation_step(batch)
+    assert isinstance(loss, torch.Tensor)
+    '''
+
+def test_optimizers():
+    model = HatespeechClassification() # Do we need to test for all types of models?
+    optimizer = model.configure_optimizers()
+    
+    # Ensure optimizer is of the correct type
+    assert isinstance(optimizer, torch.optim.Optimizer)
